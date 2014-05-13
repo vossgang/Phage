bb29642c13966b5589a87ff33f42b83036a1ef42
Anton Rivera <anton.rivera@gmail.com>
2014-05-13 16:19:29 -0700

    storyboard

---
+++ Phage/CFGameCenterViewController.m
@@ -0,0 +1,29 @@
+//
+//  CFGameCenterViewController.m
+//  Phage
+//
+//  Created by Anton Rivera on 5/13/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import "CFGameCenterViewController.h"
+
+@interface CFGameCenterViewController ()
+
+@end
+
+@implementation CFGameCenterViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)backButtonPressed:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+
+@end
