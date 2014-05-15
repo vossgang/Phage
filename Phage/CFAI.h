e9ad7feb351590b6d2dde83b96f0ca5bd376454b
Anton Rivera <anton.rivera@gmail.com>
2014-05-15 15:28:00 -0700

    added distance method and scuttleInPath method to CFAI model

---
+++ Phage/CFAI.h
@@ -0,0 +1,17 @@
+//
+//  CFAI.h
+//  Phage
+//
+//  Created by Anton Rivera on 5/15/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import "CFPlayer.h"
+#import "CFCell.h"
+
+@interface CFAI : CFPlayer
+
+-(CGFloat)distanceBetweenCellLocation:(CFCell *)location andDestinationCell:(CFCell *)destinationCell;
+-(BOOL)isScuttle:(CFCell *)scuttle inPathBetweenCellLocation:(CFCell *)location andDestinationCell:(CFCell *)destinationCell;
+
+@end
