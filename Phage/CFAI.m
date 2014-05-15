c236f8fd94b46f9e8e37c3dcbc47a0d8af83b1d1
vossgang <vossgang@gmail.com>
2014-05-15 15:55:53 -0700

    only ONE CFAI

--- Phage/CFAI.m
+++ Phage/CFAI.m
@@ -7,6 +7,9 @@
 //
 
 #import "CFAI.h"
+#import "CFMyScene.h"
+
+#define k_RANDOMIZATION_VALUE 10
 
 @implementation CFAI
 
@@ -36,5 +39,18 @@
     
     return YES;
 }
+
+- (double)createRandomizationValue:(double)withValue {
+    // Generate a random value between -10% and 10%, then modify the incomming value by the random value
+    // and return it to the AI method calling this
+    return withValue + (withValue * ((1.0f * ((float)arc4random() / (float)RAND_MAX) - 1.0f) * k_RANDOMIZATION_VALUE / 100));
+}
+
+- (void)obtainCellInformation {
+    CFMyScene *myScene = [CFMyScene new];
+    [myScene returnCellInfoToAI];
+    
+}
+
 
 @end
